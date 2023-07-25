5bc6d243ab777f759acfed3a9e35e47b478be081
2007luke <lukaswolff.aid@outlook.de>
2023-07-25 19:39:38 +0200

    Function ics for all finished, 1/3  js  commented

--- TodoApi/wwwroot/js/site.js
+++ TodoApi/wwwroot/js/site.js
@@ -3,40 +3,37 @@
 const uri = 'api/Todo';
 let todos = [];
 
-
 function getItems() {
-  fetch(uri)
+
+  fetch(uri)      //get Items from Server
     .then(response => response.json())
     .then(data => _displayItems(data))
-    .catch(error => console.error('Unable to get items.', error));
-    
-}
-function modaladd2() {
-  /*date = new Date();
-  year = date.getFullYear();
-  month = date.getMonth() + 1;
-  day = date.getDate();
-  date2 = year + "-" + month + "-" + day;
-  // String(date2);*/
-  datebox1 = document.getElementById("datebox");
-  // datebox1.setAttribute('value', date2);
-  addmodaö = document.getElementById("addtodo");
-  // var teste = datebox1.value;
-  addmodaö.style.display = "block";
+    .catch(error => console.error('Unable to get items.', error)); //error when server problem
 
 }
+
+function displayaddform() {
+
+  datebox1 = document.getElementById("datebox");      //get html elements
+  addform = document.getElementById("addtodo");
+  addform.style.display = "block";      //display modal dialog form
+}
+
 function addItem() {
-  const addNameTextbox = document.getElementById('add-name');
-  if (addNameTextbox.value == null || addNameTextbox.value.trim() == "") {
-    var fehlerdiv = document.getElementById('eingabefehler');
-    fehlerdiv.textContent = "Please enter a Name";
+  const addNameTextbox = document.getElementById('add-name');     //get html textbox
+
+  if (addNameTextbox.value == null || addNameTextbox.value.trim() == "") {      //security check if textbox is empty
+    var fehlerdiv = document.getElementById('eingabefehler');     //get html error text
+    fehlerdiv.textContent = "Please enter a Name";      //set error text
     return;
-  } else {
+  } else {        //hide if all correct
     var fehlerdiv = document.getElementById('eingabefehler');
     fehlerdiv.textContent = "";
   }
-  datebox1 = document.getElementById("datebox");
-  if (datebox1.value == null || datebox1.value == "") {
+
+  datebox1 = document.getElementById("datebox");      // get html datebox
+
+  if (datebox1.value == null || datebox1.value == "") {     //same security check as in line 20
     var fehlerdivdate = document.getElementById('dateerroradd');
     fehlerdivdate.textContent = "Please enter a Date";
     return;
@@ -44,74 +41,70 @@ function addItem() {
     var fehlerdivdate = document.getElementById('dateerroradd');
     fehlerdivdate.textContent = "";
   }
-  var dateboxrow = datebox1.value;
-  var dateboxvalue = dateboxrow.split("-")
-  /*var day =  dateboxvalue[2]; // parseInt(stSplit[1],10)-1, parseInt(stSplit[0],10);
-  var month = dateboxvalue[1];
-  var year = dateboxvalue[0];*/
-  const item = {
+
+  var dateboxrow = datebox1.value;        //copy value of datebox in working variable
+  var dateboxvalue = dateboxrow.split("-")   //split value for working
+
+  const item = {      //creat item for sending to server
     isComplete: false,
-    name: addNameTextbox.value.trim(),
-    day: dateboxvalue[2], // parseInt(stSplit[1],10)-1, parseInt(stSplit[0],10);
+    name: addNameTextbox.value.trim(),  //trim the text input
+    day: dateboxvalue[2], //use data from array
     month: dateboxvalue[1],
     year: dateboxvalue[0]
   };
 
-  fetch(uri, {
-    method: 'POST',
+  fetch(uri, {      //send item to server
+    method: 'POST',   //set sending method
     headers: {
-      'Accept': 'application/json',
+      'Accept': 'application/json',  //configurate json
       'Content-Type': 'application/json'
     },
-    body: JSON.stringify(item)
+    body: JSON.stringify(item)    //creat json
   })
-    .then(response => response.json())
+    .then(response => response.json())      //get response
     .then(() => {
-      getItems();
-      addNameTextbox.value = '';
+      getItems();     //reloade items
+      addNameTextbox.value = '';      //clear input textbox
     })
-    //.catch(error => console.error('Unable to add item.', error));
-    .catch(error => alert("Error: Unable to add item. No name"));
-  addmodaö = document.getElementById("addtodo");
-  // datebox1.setAttribute('value', '');
-
-  addmodaö.style.display = "none";
+    .catch(error => alert("Error: Unable to add item. No name"));    //if something went wrong error
+    addform = document.getElementById("addtodo");     // get html modal dialog
+    addform.style.display = "none";       //hide modal dialogue
 }
 
 function deleteItem(id) {
-  fetch(`${uri}/${id}`, {
-    method: 'DELETE'
+  fetch(`${uri}/${id}`, {       //send 
+    method: 'DELETE'      //set sending method
   })
-    .then(() => getItems())
-    .catch(error => console.error('Unable to delete item.', error));
+    .then(() => getItems())     //reloade items
+    .catch(error => console.error('Unable to delete item.', error));      //if something went wrong error
 }
 
 function displayEditForm(id) {
-  el = document.getElementById("example");
-  const item = todos.find(item => item.id === id);
-  date3 = item.year + "-" + item.month + "-" + item.day;
-  document.getElementById('dateboxedit').value = date3;
-  document.getElementById('edit-name').value = item.name;
-  document.getElementById('edit-id').value = item.id;
-  document.getElementById('edit-isComplete').checked = item.isComplete;
-  document.getElementById('example').style.display = 'block';
+  el = document.getElementById("example");      //get html edit modal dialog
+  const item = todos.find(item => item.id === id);      //find the right item for editing
+  dateusable = item.year + "-" + item.month + "-" + item.day;      //set date variables in usable format
 
-
+  document.getElementById('dateboxedit').value = dateusable;      //set value of datebox
+  document.getElementById('edit-name').value = item.name;     //set value of text box
+  document.getElementById('edit-id').value = item.id;     //set item id
+  document.getElementById('edit-isComplete').checked = item.isComplete;     //set is completed or not
+  document.getElementById('example').style.display = 'block';     //display modal dialog form
 }
 
 function closeaddbox() {
-  box = document.getElementById("addtodo");
-  box.style.display = "none";
-  var fehlerdivdate4 = document.getElementById('dateerroredit');
-    fehlerdivdate4.textContent = "";
+  addbox = document.getElementById("addtodo");   //get html add modal dialog
+  addbox.style.display = "none";      //hide modal dialog form
+  var fehlerdivdate4 = document.getElementById('dateerroradd');
+  fehlerdivdate4.textContent = "";
   error = document.getElementById('eingabefehler');
   error.textContent = "";
 }
+
 function closebox() {
   var fehlerdiv2 = document.getElementById('editerror');
   fehlerdiv2.textContent = "";
   var fehlerdivdate3 = document.getElementById('dateerroredit');
-    fehlerdivdate3.textContent = "";
+  fehlerdivdate3.textContent = "";
   el = document.getElementById("example");
   el.style.display = "none";
 
@@ -176,213 +169,211 @@ function _displayCount(itemCount) {
 
   document.getElementById('counter').innerText = `${itemCount} ${name}`;
 }
-function triggerics(){
+function triggerics() {
   fetch(uri)
-  .then(response => response.json())
-  .then(data => _genicserall(data))
-  .catch(error => console.error('Unable to get items.', error));
-}/*
-function _genicserall(data){
-  //  var data;
+    .then(response => response.json())
+    .then(data => _genicserall(data))
+    .catch(error => console.error('Unable to get items.', error));
+}
+
+function closeerrorbox() {
+  errorbox = document.getElementById("erroricsall");
+  errorbox.style.display = "none";
+}
+
+function getItemsics() {
   fetch(uri)
-  .then(response => response.json())
-  // .then(data = data1)
-  .catch(error => console.error('Unable to get items.', error));
- if (data == null){
-  alert("No Item");
- }
- eventName1 = data1[0].name;
-  day0 = data1[0].day
-  month0 = data1[0].month
-  year0 = data1[0].year
-
-  // date5 = year0 + month0 + day0 + "T" + "120000Z";
- //name of file to download as
- fileName = 'my-complete-ToDo.ics';
- 
- //start time of event in iCal
-// dateStart = date5;
- 
- //end time of event in iCal
-//  dateEnd = date5;
-
-
-     //helper functions
-    
-    //iso date for ical formats
-    this._isofix = function(d){
-		  var offset = ("0"+((new Date()).getTimezoneOffset()/0)).slice(-2);
-
-	    if(typeof d=='string'){
-		    return d.replace(/\-/g, '')+'T'+offset+'0000Z';
-	    }else{
-				return d.getFullYear()+this._zp(d.getMonth()+1)+this._zp(d.getDate())+'T'+this._zp(d.getHours())+"0000Z";
-		  }
-		}
-		
-    //zero padding for data fixes
-    this._zp = function(s){ return ("0"+s).slice(-2); }
-		this._save = function(fileURL){
-		  if (!window.ActiveXObject) {
-		      var save = document.createElement('a');
-		      save.href = fileURL;
-		      save.target = '_blank';
-		      save.download = this.fileName || 'unknown';
-		
-		      var evt = new MouseEvent('click', {
-		          'view': window,
-		          'bubbles': true,
-		          'cancelable': false
-		      });
-		      save.dispatchEvent(evt);
-		
-		      (window.URL || window.webkitURL).revokeObjectURL(save.href);
-		  }
-		
-		  // for IE < 11
-		  else if ( !! window.ActiveXObject && document.execCommand)     {
-		      var _window = window.open(fileURL, '_blank');
-		      _window.document.close();
-		      _window.document.execCommand('SaveAs', true, this.fileName || fileURL)
-		      _window.close();
-		  }	
-		}    
-
-var now = new Date();
-var ics_lines = [
- "BEGIN:VCALENDAR",
- "VERSION:2.0",
- "PRODID:-//ToDoAPI",
- "METHOD:PUBLISH",
- "BEGIN:VEVENT",
- "UID:event-"+now.getTime()+"@addroid.com",
- "SUMMARY:"+this.eventName,
- "DESCRIPTION:"+this.eventName,
- "CLASS: PRIVATE",
- "DTSTART:"+dateStart,
- "DTEND:"+dateEnd,
- "DTSTAMP:"+dateStart,
- "END:VEVENT"
- 
-];
-
-var ics_lines = [];
-
-  data.forEach[ics_lines => {
-    eventName1 = ics_lines.name;
-    day0 = ics_lines.day;
-    month0 = ics_lines.month;
-    year0 = ics_lines.year;
-    date5 = year0 + month0 + day0 + "T" + "120000Z";
-  +"BEGIN:VEVENT",
-   "UID:event-"+now.getTime()+"@addroid.com",
-   "SUMMARY:"+eventName1,
-   "DESCRIPTION:"+eventName1,
-   "CLASS: PRIVATE",
-   "DTSTART:"+date5,
-   "DTEND:"+date5,
-   "DTSTAMP:"+dateStart,
-   "END:VEVENT"+date5
-  }];
-
-var dlurl = 'data:text/calendar;base64,'+btoa(ics_lines.join('\r\n'));
-
- var dlurl = 'data:text/calendar;base64,'+btoa(ics_lines.join('\r\n'));
- 
- try {
-   this._save(dlurl);
- }catch(e){
-   console.log(e);
- }
-}*/
-function genradeics(_name, _day, _month, _year){
-
-
-  date2 = _year  + _month + _day + "T" + "120000Z";
- //name of event in iCal
-eventName = _name;
-    
- //name of file to download as
- fileName = 'my-ToDo.ics';
- 
- //start time of event in iCal
-dateStart = date2;
- 
- //end time of event in iCal
- dateEnd = date2;
-
-
-     //helper functions
-    
-    //iso date for ical formats
-    /*this._isofix = function(d){
-		  var offset = ("0"+((new Date()).getTimezoneOffset()/0)).slice(-2);
-
-	    if(typeof d=='string'){
-		    return d.replace(/\-/g, '')+'T'+offset+'0000Z';
-	    }else{
-				return d.getFullYear()+this._zp(d.getMonth()+1)+this._zp(d.getDate())+'T'+this._zp(d.getHours())+"0000Z";
-		  }
-		}*/
-		
-    this._zp = function(s){ return ("0"+s).slice(-2); }
-		this._save = function(fileURL){
-		  if (!window.ActiveXObject) {
-		      var save = document.createElement('a');
-		      save.href = fileURL;
-		      save.target = '_blank';
-		      save.download = this.fileName || 'unknown';
-		
-		      var evt = new MouseEvent('click', {
-		          'view': window,
-		          'bubbles': true,
-		          'cancelable': false
-		      });
-		      save.dispatchEvent(evt);
-		
-		      (window.URL || window.webkitURL).revokeObjectURL(save.href);
-		  }
-		
-		  // for IE < 11
-		  else if ( !! window.ActiveXObject && document.execCommand)     {
-		      var _window = window.open(fileURL, '_blank');
-		      _window.document.close();
-		      _window.document.execCommand('SaveAs', true, this.fileName || fileURL)
-		      _window.close();
-		  }	
-		}    
-
-var now = new Date();
-var ics_lines2 = [
- "BEGIN:VCALENDAR",
- "VERSION:2.0",
- "PRODID:-//ToDoAPI",
- "METHOD:PUBLISH",
- "BEGIN:VEVENT",
- "UID:event-"+now.getTime()+"@addroid.com",
- "SUMMARY:"+this.eventName,
- "DESCRIPTION:"+this.eventName,
- "CLASS: PRIVATE",
- "DTSTART:"+dateStart,
- "DTEND:"+dateEnd,
- "DTSTAMP:"+dateStart,
- "END:VEVENT",
- "END:VCALENDAR"
-];
-
-var dlurl = 'data:text/calendar;base64,'+btoa(ics_lines2.join('\r\n'));
-
- var dlurl = 'data:text/calendar;base64,'+btoa(ics_lines2.join('\r\n'));
- 
- try {
-   this._save(dlurl);
- }catch(e){
-   console.log(e);
- }
-  
+    .then(response => response.json())
+    .then(data => _genicserall(data))
+    .catch(error => console.error('Unable to get items.', error));
 
 }
-  
+function _genicserall(data) {
+  //  var data;
+
+  if (data == null || data.length == 0) {
+    errorbox = document.getElementById("erroricsall");
+    errorbox.style.display = "block";
+    return;
+  }
+
+  /*eventName1 = data1.item[0].name;
+  day0 = data1[0].day
+  month0 = data1[0].month
+  year0 = data1[0].year*/
+
+  // date5 = year0 + month0 + day0 + "T" + "120000Z";
+  //name of file to download as
+  fileName = 'my-complete-ToDo.ics';
+
+  //start time of event in iCal
+  // dateStart = date5;
+
+  //end time of event in iCal
+  //  dateEnd = date5;
+
+
+  //helper functions
+
+  //iso date for ical formats
+  this._isofix = function (d) {
+    var offset = ("0" + ((new Date()).getTimezoneOffset() / 0)).slice(-2);
+
+    if (typeof d == 'string') {
+      return d.replace(/\-/g, '') + 'T' + offset + '0000Z';
+    } else {
+      return d.getFullYear() + this._zp(d.getMonth() + 1) + this._zp(d.getDate()) + 'T' + this._zp(d.getHours()) + "0000Z";
+    }
+  }
+
+  //zero padding for data fixes
+  this._zp = function (s) { return ("0" + s).slice(-2); }
+  this._save = function (fileURL) {
+    if (!window.ActiveXObject) {
+      var save = document.createElement('a');
+      save.href = fileURL;
+      save.target = '_blank';
+      save.download = this.fileName || 'unknown';
+
+      var evt = new MouseEvent('click', {
+        'view': window,
+        'bubbles': true,
+        'cancelable': false
+      });
+      save.dispatchEvent(evt);
+
+      (window.URL || window.webkitURL).revokeObjectURL(save.href);
+    }
+
+    // for IE < 11
+    else if (!!window.ActiveXObject && document.execCommand) {
+      var _window = window.open(fileURL, '_blank');
+      _window.document.close();
+      _window.document.execCommand('SaveAs', true, this.fileName || fileURL)
+      _window.close();
+    }
+  }
+
+  var now = new Date();
+  var ics_lines = [
+    "BEGIN:VCALENDAR",
+    "VERSION:2.0",
+    "PRODID:-//ToDoAPI",
+    "METHOD:PUBLISH",
+  ];
+
+
+  data.forEach(item => {
+    summary = "SUMMARY:" + item.name;
+    decrip = "DESCRIPTION:" + item.name;
+    day0 = item.day;
+    month0 = item.month;
+    year0 = item.year;
+    dateics = year0 + month0 + day0 + "T" + "120000Z";
+    dtstart = "DTSTART:" + dateics;
+    dtend = "DTEND:" + dateics;
+    dtstamp = "DTSTAMP:" + dateics;
+    uide = "UID:event-" + now.getTime() + "@addroid.com";
+    ics_lines.push("BEGIN:VEVENT", uide, summary, decrip, "CLASS: PRIVATE", dtstart, dtend, dtstamp, "END:VEVENT");
+    summary = null;
+    decrip = null;
+    day0 = null;
+    month0 = null;
+    year0 = null;
+    dateics = null;
+    dtstart = null;
+    dtend = null;
+    dtstamp = null;
+    uide = null;
+    ics_linesitem = null;
+  });
+  ics_lines.push("END:VCALENDAR");
+  var dlurl = 'data:text/calendar;base64,' + btoa(ics_lines.join('\r\n'));
+
+  var dlurl = 'data:text/calendar;base64,' + btoa(ics_lines.join('\r\n'));
+
+  try {
+    this._save(dlurl);
+  } catch (e) {
+    console.log(e);
+  }
+}
+function genradeics(_name, _day, _month, _year) {
+
+
+  date2 = _year + _month + _day + "T" + "120000Z";
+  //name of event in iCal
+  eventName = _name;
+
+  //name of file to download as
+  fileName = 'my-ToDo.ics';
+
+  //start time of event in iCal
+  dateStart = date2;
+
+  //end time of event in iCal
+  dateEnd = date2;
+
+
+
+  this._zp = function (s) { return ("0" + s).slice(-2); }
+  this._save = function (fileURL) {
+    if (!window.ActiveXObject) {
+      var save = document.createElement('a');
+      save.href = fileURL;
+      save.target = '_blank';
+      save.download = this.fileName || 'unknown';
+
+      var evt = new MouseEvent('click', {
+        'view': window,
+        'bubbles': true,
+        'cancelable': false
+      });
+      save.dispatchEvent(evt);
+
+      (window.URL || window.webkitURL).revokeObjectURL(save.href);
+    }
+
+    // for IE < 11
+    else if (!!window.ActiveXObject && document.execCommand) {
+      var _window = window.open(fileURL, '_blank');
+      _window.document.close();
+      _window.document.execCommand('SaveAs', true, this.fileName || fileURL)
+      _window.close();
+    }
+  }
+
+  var now = new Date();
+  var ics_lines2 = [
+    "BEGIN:VCALENDAR",
+    "VERSION:2.0",
+    "PRODID:-//ToDoAPI",
+    "METHOD:PUBLISH",
+    "BEGIN:VEVENT",
+    "UID:event-" + now.getTime() + "@addroid.com",
+    "SUMMARY:" + this.eventName,
+    "DESCRIPTION:" + this.eventName,
+    "CLASS: PRIVATE",
+    "DTSTART:" + dateStart,
+    "DTEND:" + dateEnd,
+    "DTSTAMP:" + dateStart,
+    "END:VEVENT",
+    "END:VCALENDAR"
+  ];
+
+  var dlurl = 'data:text/calendar;base64,' + btoa(ics_lines2.join('\r\n'));
+
+  var dlurl = 'data:text/calendar;base64,' + btoa(ics_lines2.join('\r\n'));
+
+  try {
+    this._save(dlurl);
+  } catch (e) {
+    console.log(e);
+  }
+
+
+}
+
 
 
 function updatecheckbox(_id, _isComplete, _name, _day, _month, _year) {
@@ -416,7 +407,7 @@ function _displayItems(data) {
   _displayCount(data.length);
 
   const button = document.createElement('button');
-
+  data1 = data;
   data.forEach(item => {
     let isCompleteCheckbox = document.createElement('input');
     isCompleteCheckbox.type = 'checkbox';
